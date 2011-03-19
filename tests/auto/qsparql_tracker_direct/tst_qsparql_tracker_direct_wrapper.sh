14fa13859e263c2d81907c26366f70b1e1be2ea3
Richard Dale <richard.dale@codethink.co.uk>
2011-03-19 12:28:58 +0000

    * Make the test program set up its own test data, and don't use shell scripts

--- tests/auto/qsparql_tracker_direct/tst_qsparql_tracker_direct_wrapper.sh
+++ tests/auto/qsparql_tracker_direct/tst_qsparql_tracker_direct_wrapper.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 
-tracker-import testdata_tracker_direct.ttl
-
-if [ $? -ne 0 ] ; then
-        echo "Could not import test data"
-        exit $?
-fi
-
-numberOfContacts=2000
-
-query=`./massinsert.sh $numberOfContacts`
-# echo $query
-tracker-sparql -u --query "$query"
-
 ./tst_qsparql_tracker_direct
 er=$?
-
-tracker-sparql -u -f clean_data_tracker_direct.rq
-
-i=1
-while [ $i -le $numberOfContacts ]
-do
-    query=`./deletecontact.sh $i`
-#     echo $query
-    tracker-sparql -u --query "$query"
-    i=$(($i+1))
-done
 
 exit $er
